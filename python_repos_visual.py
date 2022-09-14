3483bba5f692b496ec09005ea6f8026c279d4ecf
404rorre <404rorre.edoc@gmail.com>
2022-09-14 20:00:00 +0200

    created first visual for repos

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -1,45 +1,49 @@
 import json
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
+def dump_json (json_dump):
+	"""
+	Dumps api call in json file.
+	(not included inside the tutorial, twas taken from another chapter)
+	"""
+	readable_file = "api_dump/github_apicall_visual.json"
+	with open(readable_file, "w", encoding="utf-8") as f:
+		json.dump(json_dump, f, indent=4)
 
 #Make an API call and store the responses.
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
-#Store API response in variable
+
+#Process results
 response_dict = r.json()
-
-#Visual help to read api call in indented format as file dump.
-#(not included inside the tutorial, twas taken from another chapter)
-readable_file = "api_dump/github_apicall.json"
-with open(readable_file, "w", encoding="utf-8") as f:
-	json.dump(response_dict, f, indent=4)
-
-print(f"Total repositories: {response_dict['total_count']}")
-
-#Explore information about the repositories.
 repo_dicts = response_dict["items"]
-print(f"Repositories returned: {len(repo_dicts)}")
+dump_json(response_dict)
+repo_names, stars = [], []
 
-#Examine the first repository.
-# repo_dict = repo_dicts[0]
-# print(f"\nKeys: {len(repo_dict)}")
-# for key in sorted(repo_dict.keys()):
-# 	print(key)
-
-
-#Helper 
-#Control results.
-# print(response_dict.keys())
-# print(len(response_dict["items"]))
-
-print("\nSelected information about first repository:")
 for repo_dict in repo_dicts:
-	print(f"Name: {repo_dict['name']}")
-	print(f"Owner: {repo_dict['owner']['login']}")
-	print(f"Stars: {repo_dict['stargazers_count']}")
-	print(f"Repository: {repo_dict['html_url']}")
-	print(f"Created: {repo_dict['created_at']}")
-	print(f"Updated: {repo_dict['updated_at']}")
-	print(f"Description: {repo_dict['description']}")
-	print("\n")
+	repo_names.append(repo_dict["name"])
+	stars.append(repo_dict["stargazers_count"])
+
+#Make visualization
+data = [{
+	"type" : "bar",
+	"x" : repo_names,
+	"y" : stars
+	}]
+
+my_layout = {
+	"title" : "Most-Starred Pyhton Projects on GitHub",
+	"xaxis" : {"title" : "Repository"},
+	"yaxis" : {"title" : "Stars"}
+	}
+
+fig = {
+	"data" : data,
+	"layout" : my_layout,
+	}
+
+offline.plot(fig, filename="python_repos.html")
