1e15fa233ccf310aa14c6efe176ec231c0bcfdd0
404rorre <404rorre.edoc@gmail.com>
2022-09-15 15:54:04 +0200

    created hovertext with owner and description

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -22,17 +22,22 @@ print(f"Status code: {r.status_code}")
 response_dict = r.json()
 repo_dicts = response_dict["items"]
 dump_json(response_dict)
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 
 for repo_dict in repo_dicts:
 	repo_names.append(repo_dict["name"])
 	stars.append(repo_dict["stargazers_count"])
+	owner = repo_dict["owner"]["login"]
+	description = repo_dict["description"]
+	label = f"{owner}<br />{description}"
+	labels.append(label)
 
 #Make visualization
 data = [{
 	"type" : "bar",
 	"x" : repo_names,
 	"y" : stars,
+	"hovertext" : labels,
 	"marker" : {
 		"color" : "rgb(60, 100, 150)",
 		"line" : {
